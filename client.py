ab8ea9a796024c76e34d3848d0b688f0cb99f08a
qianyiwang <stevenwang473328@gmail.com>
2017-06-08 15:23:10 -0400

    delete client

--- client.py
+++
@@ -1,8 +0,0 @@
-import sys
-import json
-import requests
-
-conv = {'Name': 'Temperature', 'Data': '25'}
-s = json.dumps(conv)
-res = requests.post("http://127.0.0.1:5000/receiveData/", json=s).json()
-print type(res)
